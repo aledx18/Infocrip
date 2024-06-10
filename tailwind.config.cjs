55e0bc9f6b60b47a79661e32c5532c505c905bb2
aledx18 <alediiaz18@gmail.com>
2024-06-10 19:14:36 -0300

    First commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,17 +2,6 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
-      },
-    },
-  },
+  theme: {},
   plugins: [require("@tailwindcss/typography")],
 };
